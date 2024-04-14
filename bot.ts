e0e692b2ca0ac80d5fe75e3ecdb9685db916efcd
Janek Szynal <jan.szynal@gmail.com>
2024-04-14 17:08:32 +0200

    Add terminal command running

--- bot.ts
+++ bot.ts
@@ -7,9 +7,23 @@ const token = Deno.env.get("BOT_TOKEN")!;
 const bot = new Bot(token); 
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-bot.command("photo", (ctx) => { 
-    const photo = InputMediaBuilder.photo(new InputFile("test_image.jpg"))
-    ctx.replyWithMediaGroup([photo]);
+bot.command("photo", async (ctx) => {
+  // Run a terminal command and await end: ffmpeg -f v4l2 -i /dev/video0 -frames 1 ~/Desktop/dx.jpg
+
+  // define command used to create the subprocess
+  const command = new Deno.Command(Deno.execPath(), {
+    args: ["eval", "console.log('hello'); console.error('world')"],
+  });
+
+  // create subprocess and collect output
+  const { code, stdout, stderr } = await command.output();
+
+  console.assert(code === 0);
+  console.assert("world\n" === new TextDecoder().decode(stderr));
+  console.log(new TextDecoder().decode(stdout));
+
+  const photo = InputMediaBuilder.photo(new InputFile("test_image.jpg"));
+  ctx.replyWithMediaGroup([photo]);
 });
 
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
