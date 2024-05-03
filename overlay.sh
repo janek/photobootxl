61ee86ba3b62a18b1455c0e789ad694b13bb9845
Janek Szynal <jan.szynal@gmail.com>
2024-05-03 19:13:24 +0200

    Add cropping 20px

--- overlay.sh
+++ overlay.sh
@@ -6,6 +6,7 @@ SCALE_FACTOR=$(shuf -i 30-90 -n 1)
 ROTATE_ANGLE=$(awk -v min=-180 -v max=180 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
 ROTATE_RAD=$(awk -v deg=$ROTATE_ANGLE 'BEGIN{print deg*3.14159/180}')
 
+# Overlay the image and then crop the bottom 20px
 ffmpeg -y -i photo.jpg -i assets/turbulence_portal.png -filter_complex \
 "[1:v]format=rgba,scale=iw*$SCALE_FACTOR/100:ih*$SCALE_FACTOR/100,rotate=$ROTATE_RAD:c=none:ow=rotw($ROTATE_RAD):oh=roth($ROTATE_RAD),colorchannelmixer=aa=0.2[fg]; \
- [0:v][fg]overlay=(W-w)/2:(H-h)/2" photo.jpg
+ [0:v][fg]overlay=(W-w)/2:(H-h)/2,crop=in_w:in_h-20:0:0" photo.jpg
