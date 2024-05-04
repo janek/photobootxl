af7fe33862d687f8751d4075f34f1fdb5dd6de77
Janek Szynal <jan.szynal@gmail.com>
2024-05-04 19:11:22 +0200

    Add displacement

--- overlay.sh
+++ overlay.sh
@@ -6,10 +6,14 @@ SCALE_FACTOR=$(shuf -i 30-90 -n 1)
 ROTATE_ANGLE=$(awk -v min=-180 -v max=180 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
 ROTATE_RAD=$(awk -v deg=$ROTATE_ANGLE 'BEGIN{print deg*3.14159/180}')
 
+# Generate random displacement for x and y axis
+DISPLACE_X=$(awk -v min=-200 -v max=200 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+DISPLACE_Y=$(awk -v min=-200 -v max=200 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+
 # Get the photo filename from the command line arguments
 PHOTO_FILENAME=$1
 
 # Overlay the image and then crop the bottom 20px
 ffmpeg -y -i $PHOTO_FILENAME -i assets/turbulence_portal.png -filter_complex \
 "[1:v]format=rgba,scale=iw*$SCALE_FACTOR/100:ih*$SCALE_FACTOR/100,rotate=$ROTATE_RAD:c=none:ow=rotw($ROTATE_RAD):oh=roth($ROTATE_RAD),colorchannelmixer=aa=0.2[fg]; \
- [0:v][fg]overlay=(W-w)/2:(H-h)/2,crop=in_w:in_h-20:0:0" $PHOTO_FILENAME
+ [0:v][fg]overlay=(W-w)/2+$DISPLACE_X:(H-h)/2+$DISPLACE_Y,crop=in_w:in_h-20:0:0" $PHOTO_FILENAME
