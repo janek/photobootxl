a976e7387d39aa950d7eec47b05d213965d6eb75
Janek Szynal <jan.szynal@gmail.com>
2024-05-05 19:58:49 +0200

    gpio_b py update

--- gpio_button_listener.py
+++ gpio_button_listener.py
@@ -10,12 +10,12 @@ try:
     while True:
         button_state = GPIO.input(11)
         if button_state == False:
-            print("Button Pressed!")
+            print("Button Pressed11")
             os.system("echo hishell")
-            os.system("deno printFromButton.ts")
+            os.system("deno run printFromButton.ts")
         else:
             print("Button Released!")
-        time.sleep(1)  # Delay for debounce and to reduce CPU usage
+        time.sleep(0.1)  # Delay for debounce and to reduce CPU usage
 
 finally:
     GPIO.cleanup()  # Clean up GPIO on normal exit
