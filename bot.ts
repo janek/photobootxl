0634268773f7131a1bca1f5123971b120824eba4
Janek Szynal <jan.szynal@gmail.com>
2024-05-05 20:11:08 +0200

    Base changes by phind

--- bot.ts
+++ bot.ts
@@ -1,9 +1,15 @@
+// bot.ts
 import {
   Bot,
   InputFile,
   InputMediaBuilder,
 } from "https://deno.land/x/grammy@v1.22.4/mod.ts";
 import { load } from "https://deno.land/std@0.221.0/dotenv/mod.ts";
+import {
+  takePhoto,
+  overlayTurbulenceOnPhoto,
+  tileAndPrintPhotos,
+} from "./photo_utils.ts";
 
 await load({ export: true });
 
@@ -44,48 +50,6 @@ export const sendLastPhotosToAdmin = async () => {
 
 export const sendMessageToAdmin = async (message: string) => {
   bot.api.sendMessage(adminId, message);
-};
-
-export const overlayTurbulenceOnPhoto = async (photoFilename: string) => {
-  const command = new Deno.Command("sh", {
-    args: ["overlay.sh", photoFilename],
-  });
-  const { code } = await command.output();
-  if (code !== 0) {
-    throw new Error(`fswebcam exited with code ${code}`);
-  }
-};
-
-export const tileAndPrintPhotos = async () => {
-  const tileCommand = new Deno.Command("sh", {
-    args: ["tile.sh"],
-  });
-  const { code: tileCode } = await tileCommand.output();
-  if (tileCode !== 0) {
-    throw new Error(`tile.sh exited with code ${tileCode}`);
-  }
-
-  const printCommand = new Deno.Command("sh", {
-    args: ["print.sh"],
-  });
-  const { code: printCode } = await printCommand.output();
-  if (printCode !== 0) {
-    throw new Error(`print.sh exited with code ${printCode}`);
-  }
-};
-
-export const takePhoto = async (photoFilename: string) => {
-  const command = new Deno.Command("fswebcam", {
-    args: [photoFilename],
-  });
-  const output = await command.output();
-  if (output.code !== 0) {
-    throw new Error(
-      `fswebcam exited with code ${
-        output.code
-      } and error message: ${new TextDecoder().decode(output.stderr)}`
-    );
-  }
 };
 
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
