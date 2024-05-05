05730759d1914a9e66d0880b694bff37df857c96
Janek Szynal <jan.szynal@gmail.com>
2024-05-05 20:17:23 +0200

    btn pr

--- printFromButton.ts
+++ printFromButton.ts
@@ -9,7 +9,7 @@ import {
 for await (const i of Array(4).keys()) {
   //   sendMessageToAdmin(`${i + 1}!`);
   console.log(`Taking photo from button - ${i + 1}!`);
-  const photoFilename = `button_photo${i}.jpg`;
+  const photoFilename = `photo${i}.jpg`;
   await takePhoto(photoFilename);
   await overlayTurbulenceOnPhoto(photoFilename);
   //   const photo = InputMediaBuilder.photo(new InputFile(photoFilename));
