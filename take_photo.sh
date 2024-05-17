067ed42bd2fa34d7ce5a8663d3ce50307147221c
Janek Szynal <jan.szynal@gmail.com>
2024-05-17 11:06:03 +0200

    Check os type before taking photo

--- take_photo.sh
+++ take_photo.sh
@@ -4,4 +4,11 @@ if [ -f "$photoFilename" ]; then
   rm "$photoFilename"
 fi
 
-fswebcam --no-banner "$photoFilename"
+# Check if the script is running on a Mac
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Use imagesnap for Mac
+    imagesnap "$photoFilename"
+else
+    # Use fswebcam for other systems
+    fswebcam --no-banner "$photoFilename"
+fi
